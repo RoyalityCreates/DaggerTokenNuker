106f307a67bfbf6c721d4f51a333ede20e36f7bb
RoyalityCreates <91426559+RoyalityCreates@users.noreply.github.com>
2021-09-27 17:09:00 +0100

    Add files via upload

---
+++ Install (Run Before Nuker).bat
@@ -0,0 +1,6 @@
+@echo off
+echo Installing all packages...
+pip install colorama discord requests datetime selenium
+cls
+echo Installed all packages please press any key to close this window.
+pause >NUL
